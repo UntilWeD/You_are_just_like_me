1809ee49dbc02a2a5be4e9eeeb36a1113ad303e2
Glados <muojeso90@gmail.com>
2025-03-01 20:28:39 +0900

    First Commit

---
+++ src/main/java/com/team/youarelikemetoo/YouAreLikeMeTooApplication.java
@@ -0,0 +1,13 @@
+package com.team.youarelikemetoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouAreLikeMeTooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouAreLikeMeTooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team/youarelikemetoo/YouAreLikeMeTooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.youarelikemetoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouAreLikeMeTooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
