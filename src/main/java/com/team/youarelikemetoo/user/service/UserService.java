610c8e1405376ccca222611353d0418931d2be8c
Glados <muojeso90@gmail.com>
2025-04-30 09:40:34 +0900

    [FIX] AZURE 환경 바꾸고 다시 Webhook 테스트

--- src/main/java/com/team/youarelikemetoo/user/service/UserService.java
+++ src/main/java/com/team/youarelikemetoo/user/service/UserService.java
@@ -37,7 +37,7 @@ public class UserService {
         Optional<UserEntity> user = userRepository.findByOauthId(oauthId);
         user.get().changeUserInfo(userDTO);
 
-        return ResponseEntity.status(HttpStatus.OK).body("아 킥킥");
+        return ResponseEntity.status(HttpStatus.OK).body("save User OK");
     }
 
 }
